0e2f6daada34ebdfcca0138c3da4da7560caffb3
LEpigeon888 <LEpigeon888@gmail.com>
2015-12-04 22:37:39 +0100

    - ajout des nouveaux stickers. - passage à la version 1.19.4.

--- respawnIrc/respawnIrc.cpp
+++ respawnIrc/respawnIrc.cpp
@@ -11,7 +11,7 @@
 #include "settingTool.hpp"
 #include "styleTool.hpp"
 
-const QString respawnIrcClass::currentVersionName("v1.19.3");
+const QString respawnIrcClass::currentVersionName("v1.19.4");
 
 respawnIrcClass::respawnIrcClass(QWidget* parent) : QWidget(parent), checkUpdate(this, currentVersionName)
 {
