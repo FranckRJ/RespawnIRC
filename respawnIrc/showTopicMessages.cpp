f099fd15e06eee50862e5a5348486554f811338d
LEpigeon888 <LEpigeon888@gmail.com>
2016-01-25 14:51:12 +0100

    - correction d'un bug qui faisait poster avec le mauvais pseudo.

--- respawnIrc/showTopicMessages.cpp
+++ respawnIrc/showTopicMessages.cpp
@@ -533,7 +533,7 @@ void showTopicMessagesClass::analyzeMessages()
 
     if(replyForFirstPage != 0)
     {
-        if(replyForFirstPage->isReadable())
+        if(replyForFirstPage->isReadable() && needToSetCookies == false)
         {
             bool cookiesChanged = false;
             QList<QNetworkCookie> newCookieList = qvariant_cast<QList<QNetworkCookie> >(replyForFirstPage->header(QNetworkRequest::SetCookieHeader));
