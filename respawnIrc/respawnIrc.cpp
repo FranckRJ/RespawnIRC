e9e82269d18de3e282fb6287ac4e92f321763618
LEpigeon888 <LEpigeon888@gmail.com>
2016-08-27 01:42:16 +0200

    - passage à la version 2.7.

--- respawnIrc/respawnIrc.cpp
+++ respawnIrc/respawnIrc.cpp
@@ -22,7 +22,7 @@
 #include "settingTool.hpp"
 #include "styleTool.hpp"
 
-const QString respawnIrcClass::currentVersionName("v2.6");
+const QString respawnIrcClass::currentVersionName("v2.7");
 
 namespace
 {
