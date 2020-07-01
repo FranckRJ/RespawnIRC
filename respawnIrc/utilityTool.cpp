7ff84f789c389c93300bb9e224eb6746161906cd
FranckRJ <franck.rj.w@gmail.com>
2020-07-01 13:35:08 +0200

    Suppression de code inutile.

--- respawnIrc/utilityTool.cpp
+++ respawnIrc/utilityTool.cpp
@@ -31,60 +31,7 @@ QList<QNetworkCookie> utilityTool::cookieToCookieList(const QNetworkCookie& conn
 
 void utilityTool::setupWebNotifierCookie(QWebEngineCookieStore& webEngineCookieStore)
 {
-    {
-        QNetworkCookie cookie("_cmpQcif3pcsupported", "1");
-        cookie.setDomain("www.jeuxvideo.com");
-        cookie.setPath("/");
-        cookie.setHttpOnly(false);
-        cookie.setSecure(true);
-        cookie.setExpirationDate(QDateTime());
-        webEngineCookieStore.setCookie(cookie);
-    }
-    {
-        QNetworkCookie cookie("_gcl_au", "1.1.68642818.1593516826");
-        cookie.setDomain("jeuxvideo.com");
-        cookie.setPath("/");
-        cookie.setHttpOnly(false);
-        cookie.setSecure(false);
-        cookie.setExpirationDate(QDateTime::currentDateTime().addMonths(3));
-        webEngineCookieStore.setCookie(cookie);
-    }
-    {
-        QNetworkCookie cookie("euconsent", "BO1z189O1z189AKAiCENDQAAAAAweAAA");
-        cookie.setDomain("www.jeuxvideo.com");
-        cookie.setPath("/");
-        cookie.setHttpOnly(false);
-        cookie.setSecure(true);
-        cookie.setExpirationDate(QDateTime::currentDateTime().addYears(1).addMonths(1));
-        webEngineCookieStore.setCookie(cookie);
-    }
-    {
-        QNetworkCookie cookie("googlepersonalization", "O1z189O1z189AA");
-        cookie.setDomain("www.jeuxvideo.com");
-        cookie.setPath("/");
-        cookie.setHttpOnly(false);
-        cookie.setSecure(true);
-        cookie.setExpirationDate(QDateTime::currentDateTime().addYears(1).addMonths(1));
-        webEngineCookieStore.setCookie(cookie);
-    }
-    {
-        QNetworkCookie cookie("noniabvendorconsent", "O1z189O1z189AKAiAA8AAA");
-        cookie.setDomain("www.jeuxvideo.com");
-        cookie.setPath("/");
-        cookie.setHttpOnly(false);
-        cookie.setSecure(true);
-        cookie.setExpirationDate(QDateTime::currentDateTime().addYears(1).addMonths(1));
-        webEngineCookieStore.setCookie(cookie);
-    }
-    {
-        QNetworkCookie cookie("visitor_country", "FR");
-        cookie.setDomain("www.jeuxvideo.com");
-        cookie.setPath("/");
-        cookie.setHttpOnly(false);
-        cookie.setSecure(false);
-        cookie.setExpirationDate(QDateTime::currentDateTime().addYears(1));
-        webEngineCookieStore.setCookie(cookie);
-    }
+    // pk sa marsh pa, ri1 ki marsh :-(
 }
 
 //blabla inutile je sais mais c'est plus beau
