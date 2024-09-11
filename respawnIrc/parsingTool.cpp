949dcb50e74ed37f0ae9ef5b89f4e01da13bf19d
Spriana <foloff@proton.me>
2024-09-11 23:59:51 +0200

    Refactor de la regex `expForTopicLinkNumber` avec captures nommées

--- respawnIrc/parsingTool.cpp
+++ respawnIrc/parsingTool.cpp
@@ -45,7 +45,7 @@ namespace
     const QRegularExpression expForMessage(R"rgx(<div class="bloc-contenu">[^<]*<div class="txt-msg  text-[^-]*-forum ">((.*?)(?=<div class="info-edition-msg">)|(.*?)(?=<div class="signature-msg)|(.*)))rgx", configDependentVar::regexpBaseOptions | QRegularExpression::DotMatchesEverythingOption);
     const QRegularExpression expForEdit(R"rgx(<div class="info-edition-msg">[^M]*Message édité le ([^ ]* [^ ]* [^ ]* [^ ]* ([0-9:]*)) par <span)rgx", configDependentVar::regexpBaseOptions);
     const QRegularExpression expForSignature(R"rgx(<div class="signature-msg[^"]*">(.*))rgx", configDependentVar::regexpBaseOptions | QRegularExpression::DotMatchesEverythingOption);
-    const QRegularExpression expForTopicLinkNumber(R"rgx((https?://([^/]*)/forums/[^-]*-([^-]*)-([^-]*)-)([^-]*)(-[^-]*-[^-]*-[^-]*-[^\.]*\.htm))rgx", configDependentVar::regexpBaseOptions);
+    const QRegularExpression expForTopicLinkNumber(R"rgx((?<beforeTopicPage>https?://(?<domain>[^/]*)/forums/[^-]*-(?<forumId>[^-]*)-(?<topicId>[^-]*)-)(?<topicPage>[^-]*)(?<afterTopicPage>-[^-]*-[^-]*-[^-]*-[^\.]*\.htm))rgx", configDependentVar::regexpBaseOptions);
     const QRegularExpression expForForumName(R"rgx(<title>(.*?)- jeuxvideo\.com</title>)rgx", configDependentVar::regexpBaseOptions);
     const QRegularExpression expForJvfLink(R"rgx(https?://jvforum\.fr/([^/]*)/([^-]*)-([^/]*))rgx", configDependentVar::regexpBaseOptions);
     const QRegularExpression expForSmiley(R"rgx(<img src="http(s)?://image\.jeuxvideo\.com/smileys_img/([^"]*)" alt="[^"]*" data-code="([^"]*)" title="[^"]*" [^>]*>)rgx", configDependentVar::regexpBaseOptions);
@@ -140,9 +140,9 @@ bool parsingTool::checkIfTopicAreSame(const QString& firstTopic, const QString& 
 
     if(matcherForFirstTopic.hasMatch() == true && matcherForSecondTopic.hasMatch() == true)
     {
-        return matcherForFirstTopic.captured(2) == matcherForSecondTopic.captured(2) &&
-               matcherForFirstTopic.captured(3) == matcherForSecondTopic.captured(3) &&
-               matcherForFirstTopic.captured(4) == matcherForSecondTopic.captured(4);
+        return matcherForFirstTopic.captured("domain") == matcherForSecondTopic.captured("domain") &&
+               matcherForFirstTopic.captured("forumId") == matcherForSecondTopic.captured("forumId") &&
+               matcherForFirstTopic.captured("topicId") == matcherForSecondTopic.captured("topicId");
     }
     else
     {
@@ -350,7 +350,7 @@ QString parsingTool::getFirstPageOfTopic(const QString& topicLink)
 
     if(matchForFirstPage.hasMatch() == true)
     {
-        return matchForFirstPage.captured(1) + "1" + matchForFirstPage.captured(6);
+        return matchForFirstPage.captured("beforeTopicPage") + "1" + matchForFirstPage.captured("afterTopicPage");
     }
     else
     {
@@ -361,11 +361,11 @@ QString parsingTool::getFirstPageOfTopic(const QString& topicLink)
 QString parsingTool::getBeforeLastPageOfTopic(const QString& topicLink)
 {
     QRegularExpressionMatch matchForBeforeLastPage = expForTopicLinkNumber.match(topicLink);
-    QString pageNumber = matchForBeforeLastPage.captured(5);
+    QString pageNumber = matchForBeforeLastPage.captured("topicPage");
 
     if(pageNumber.isEmpty() == false && pageNumber != "1")
     {
-        return matchForBeforeLastPage.captured(1) + QString::number(pageNumber.toInt() - 1) + matchForBeforeLastPage.captured(6);
+        return matchForBeforeLastPage.captured("beforeTopicPage") + QString::number(pageNumber.toInt() - 1) + matchForBeforeLastPage.captured("afterTopicPage");
     }
     else
     {
@@ -483,7 +483,7 @@ QString parsingTool::getForumOfTopic(const QString& topicLink)
 
     if(infosMatcher.hasMatch() == true)
     {
-        return "https://" + infosMatcher.captured(2) + "/forums/0-" + infosMatcher.captured(3) + "-0-1-0-1-0-respawn-irc.htm";
+        return "https://" + infosMatcher.captured("domain") + "/forums/0-" + infosMatcher.captured("forumId") + "-0-1-0-1-0-respawn-irc.htm";
     }
     else
     {
